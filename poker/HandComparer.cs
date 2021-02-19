3b5fa68c80a8e3fd40c87929d17902865b99fad2
Stefc <stefan.boether@gmail.com>
2021-02-19 08:26:24 +0100

    HandComparer folgt Solid Prinzip

--- poker/HandComparer.cs
+++ poker/HandComparer.cs
@@ -7,32 +7,30 @@ using Hand = System.Collections.Generic.IEnumerable<Card>;
 
 public class HandComparer : IComparer<Hand>, IEqualityComparer<Hand>
 {
-    private readonly bool compareStraight;
-    private static readonly Lazy<HandComparer> @default = new Lazy<HandComparer>(() => new HandComparer(false), true);
-    private static readonly Lazy<HandComparer> straight = new Lazy<HandComparer>(() => new HandComparer(true), true);
+    private readonly Func<Hand,IEnumerable<Rank>> rankOrdering;
+    private static readonly Lazy<HandComparer> @default = new Lazy<HandComparer>(() => new HandComparer(DefaultRankOrder), true);
+    private static readonly Lazy<HandComparer> straight = new Lazy<HandComparer>(() => new HandComparer(StraightRankOrder), true);
 
-    public HandComparer(bool compareStraight)
+    public HandComparer(Func<Hand,IEnumerable<Rank>> rankOrdering)
     {
-        this.compareStraight = compareStraight;
+        this.rankOrdering = rankOrdering;
     }
 
-    private IEnumerable<Rank> OrderByRank(Hand hand) =>
-        this.compareStraight && hand.HasPoorAce() ?
-            hand.Select(c => c.Rank).OrderBy(r => r).Skip(1).Append(Rank.PoorAce)
-            :
-            hand.Select(c => c.Rank).OrderBy(r => r);
+    private static IEnumerable<Rank> DefaultRankOrder(Hand hand) 
+    => hand.Select(c => c.Rank).OrderBy(r => r);
+
+    private static IEnumerable<Rank> StraightRankOrder(Hand hand)
+    => hand.HasPoorAce() ? DefaultRankOrder(hand).Skip(1).Append(Rank.PoorAce) : DefaultRankOrder(hand);
 
     public int Compare(Hand x, Hand y)
-    {
-
         // 4D 5S 6S 8D 3C", x =  8D 6S 5S 4D 3C
         // 2S 4C 7S 9H 10H" y = 10H 9H 7S 4C 2S
         // zip                   +1 +1 +1  0 -1
 
-        var match = OrderByRank(x)
-            .Zip(OrderByRank(y), (lhs, rhs) => lhs.CompareTo(rhs));
-        return (match.All(cmp => cmp == 0)) ? 0 : match.FirstOrDefault(cmp => cmp != 0);
-    }
+    => rankOrdering(x)
+        .Zip(rankOrdering(y), (leftRank, rightRank) => leftRank.CompareTo(rightRank))
+        .FirstOrDefault(cmp => cmp != 0);
+    
 
     public bool Equals(Hand x, Hand y) => this.Compare(x, y) == 0;
 
